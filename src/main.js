ed80185c2c047b5e9239cf7036b68952d0cb7d7b
Mr.cat <fantasy.lian@163.com>
2020-05-26 23:49:25 +0800

    抽出样式文件

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import './main.css'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+	el: '#app',
+	template: '<App/>',
+	components: { App }
 })
